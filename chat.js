e98457475560ab9e997ad8545d6b25587ff2bf0d
Mark Lewin <mark.lewin@vonage.com>
2019-09-30 14:47:03 +0100

    Refactor

--- chat.js
+++ chat.js
@@ -1,103 +1,91 @@
-const USER1_JWT = ''
-const USER2_JWT = ''
+const USER1_JWT = '';
+const USER2_JWT = '';
 const CONVERSATION_ID = '';
 
-let conversation = null;
-let timeout = null;
-const messageTextarea = document.getElementById('messageTextarea')
-const messageFeed = document.getElementById('messageFeed')
-const sendButton = document.getElementById('send')
-const loginForm = document.getElementById('login')
-const status = document.getElementById('status')
-
-setupUserEvents()
-
-function eventLogger(event) {
-  return () => { console.log("'%s' event was sent", event) }
-}
+const messageTextarea = document.getElementById('messageTextarea');
+const messageFeed = document.getElementById('messageFeed');
+const sendButton = document.getElementById('send');
+const loginForm = document.getElementById('login');
+const status = document.getElementById('status');
 
 function authenticate(username) {
-  if (username == "USER1") {
-    return USER1_JWT
+  if (username == "user1") {
+    return USER1_JWT;
   }
-  if (username == "USER2") {
-    return USER2_JWT
+  if (username == "user2") {
+    return USER2_JWT;
   }
-  alert("User not recognized")
+  alert("User not recognized");
 }
 
-function setupShowMessages(conv) {
-  conversation = conv
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const userToken = authenticate(document.getElementById('username').value);
+  if (userToken) {
+    document.getElementById('messages').style.display = 'block';
+    document.getElementById('login').style.display = 'none';
+    run(userToken);
+  }
+});
+
+async function run(userToken) {
+  let client = new NexmoClient({ debug: true });
+  let app = await client.login(userToken);
+  let conversation = await app.getConversation(CONVERSATION_ID);
+
+  // Update the UI to show which user we are
   document.getElementById('sessionName').innerHTML = conversation.me.user.name + "'s messages"
 
-  // Bind to events on the conversation
-  conversation.on('text', (sender, message) => {
-    const rawDate = new Date(Date.parse(message.timestamp))
+  // Load events that happened before the page loaded
+  let events = await conversation.getEvents({ event_type: "text", page_size: 100 });
+  events.items.forEach(event => {
+    addMessage(conversation.members.get(event.from), event, conversation.me);
+  });
 
-    const formattedDate = moment(rawDate).calendar()
+  // Any time there's a new text event, add it as a message
+  conversation.on('text', (sender, event) => {
+    addMessage(sender, event, conversation.me);
+  });
 
-    let text = ''
-    if (message.from !== conversation.me.id) {
-      text = `<span style="color:red">${sender.user.name} <span style="color:red">(${formattedDate}): <b>${message.body.text}</b></span><br>`
-    } else {
-      text = `me (${formattedDate}): <b>${message.body.text}</b><br>`
-    }
+  // Listen for clicks on the submit button and send the existing text value
+  sendButton.addEventListener('click', async () => {
+    await conversation.sendText(messageTextarea.value);
+    messageTextarea.value = '';
+  });
 
-    messageFeed.innerHTML = messageFeed.innerHTML + text
+  messageTextarea.addEventListener('keypress', (event) => {
+    conversation.startTyping();
+  });
 
-  })
+  var timeout = null;
+  messageTextarea.addEventListener('keyup', (event) => {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      conversation.stopTyping();
+    }, 500);
+  });
 
   conversation.on("text:typing:on", (data) => {
-    console.log(data)
     if (data.user.id !== data.conversation.me.user.id) {
-      status.innerHTML = data.name + " is typing..."
+      status.innerHTML = data.user.name + " is typing...";
     }
   });
 
   conversation.on("text:typing:off", (data) => {
-    status.innerHTML = ""
+    status.innerHTML = "";
   });
 }
 
-function joinConversation(userToken) {
-  new NexmoClient({
-    debug: false
-  })
-    .login(userToken)
-    .then(app => {
-      return app.getConversation(CONVERSATION_ID)
-    })
-    .then(setupShowMessages.bind(this))
-}
+function addMessage(sender, message, me) {
+  const rawDate = new Date(Date.parse(message.timestamp))
+  const formattedDate = moment(rawDate).calendar()
 
-function setupUserEvents() {
-  sendButton.addEventListener('click', () => {
-    conversation.sendText(messageTextarea.value).then(() => {
-      eventLogger('text')()
-      messageTextarea.value = ''
-    }).catch((error) => {
-      console.log(error)
-    })
-  })
+  let text = ''
+  if (message.from !== me.id) {
+    text = `<span style="color:red">${sender.user.name} <span style="color:red;">(${formattedDate}): <b>${message.body.text}</b></span>`
+  } else {
+    text = `me (${formattedDate}): <b>${message.body.text}</b>`
+  }
 
-  loginForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const userToken = authenticate(document.getElementById('username').value)
-    if (userToken) {
-      document.getElementById('messages').style.display = 'block'
-      document.getElementById('login').style.display = 'none'
-      joinConversation(userToken)
-    }
-  })
-
-  messageTextarea.addEventListener('keypress', (event) => {
-    conversation.startTyping()
-  })
-
-  messageTextarea.addEventListener('keyup', (event) => {
-    clearTimeout(timeout)
-    timeout = setTimeout(() => {
-      conversation.stopTyping()
-    }, 500);
-  })
+  messageFeed.innerHTML = messageFeed.innerHTML + text + '<br />';
 }
