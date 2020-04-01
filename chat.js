4ce8e01699adac972be1fd0aa3519f4aa9d4696b
Dwane <hey@dwane.io>
2020-04-01 14:27:28 -0400

    Refactored code to include pagination.

--- chat.js
+++ chat.js
@@ -8,6 +8,15 @@ const sendButton = document.getElementById('send');
 const loginForm = document.getElementById('login');
 const status = document.getElementById('status');
 
+const loadMessagesButton = document.getElementById('loadMessages');
+const messagesCountSpan = document.getElementById('messagesCount');
+const messageDateSpan = document.getElementById('messageDate');
+
+let conversation;
+let listedEvents;
+let messagesCount = 0;
+let messageDate;
+
 function authenticate(username) {
   if (username == "user1") {
     return USER1_JWT;
@@ -28,23 +37,54 @@ loginForm.addEventListener('submit', (event) => {
   }
 });
 
+loadMessagesButton.addEventListener('click', async (event) => {
+  // Get next page of events
+  let nextEvents = await listedEvents.getNext();
+  await listMessages(nextEvents);
+});
+
+async function listMessages(events) {
+  let messages = '';
+
+  // If there is a next page, display the Load Previous Messages button
+  if (events.hasNext()){
+    loadMessagesButton.style.display = "block";
+  } else {
+    loadMessagesButton.style.display = "none";
+  }
+
+  // Replace current with new page of events
+  listedEvents = events;
+
+  events.items.forEach(event => {
+    const formattedMessage = formatMessage(conversation.members.get(event.from), event, conversation.me);
+    messages = formattedMessage + messages;
+  });
+
+  // Update UI
+  messageFeed.innerHTML = messages + messageFeed.innerHTML;
+  messagesCountSpan.textContent = `${messagesCount}`;
+  messageDateSpan.textContent = messageDate;
+};
+
 async function run(userToken) {
   let client = new NexmoClient({ debug: true });
   let app = await client.login(userToken);
-  let conversation = await app.getConversation(CONVERSATION_ID);
+  conversation = await app.getConversation(CONVERSATION_ID);
 
   // Update the UI to show which user we are
-  document.getElementById('sessionName').innerHTML = conversation.me.user.name + "'s messages"
+  document.getElementById('sessionName').innerHTML = conversation.me.user.name + "'s messages";
 
   // Load events that happened before the page loaded
-  let events = await conversation.getEvents({ event_type: "text", page_size: 100 });
-  events.items.forEach(event => {
-    addMessage(conversation.members.get(event.from), event, conversation.me);
-  });
+  let initialEvents = await conversation.getEvents({ event_type: "text", page_size: 10, order:"desc" });
+
+  await listMessages(initialEvents);
 
   // Any time there's a new text event, add it as a message
   conversation.on('text', (sender, event) => {
-    addMessage(sender, event, conversation.me);
+    const formattedMessage = formatMessage(sender, event, conversation.me);
+    messageFeed.innerHTML = messageFeed.innerHTML +  formattedMessage;
+    messagesCountSpan.textContent = `${messagesCount}`;
   });
 
   // Listen for clicks on the submit button and send the existing text value
@@ -53,39 +93,46 @@ async function run(userToken) {
     messageTextarea.value = '';
   });
 
+  // Listen for key presses and send start typing event
   messageTextarea.addEventListener('keypress', (event) => {
     conversation.startTyping();
   });
 
-  var timeout = null;
+  // Listen for when typing stops and send an event
+  let timeout = null;
   messageTextarea.addEventListener('keyup', (event) => {
-    clearTimeout(timeout)
+    clearTimeout(timeout);
     timeout = setTimeout(() => {
       conversation.stopTyping();
     }, 500);
   });
 
+  // When there is a typing event, display an indicator
   conversation.on("text:typing:on", (data) => {
     if (data.user.id !== data.conversation.me.user.id) {
       status.innerHTML = data.user.name + " is typing...";
     }
   });
 
+  // When typing stops, clear typing indicator
   conversation.on("text:typing:off", (data) => {
     status.innerHTML = "";
   });
 }
 
-function addMessage(sender, message, me) {
-  const rawDate = new Date(Date.parse(message.timestamp))
-  const formattedDate = moment(rawDate).calendar()
+function formatMessage(sender, message, me) {
+  const rawDate = new Date(Date.parse(message.timestamp));
+  const formattedDate = moment(rawDate).calendar();
+  let text = '';
+  messagesCount++;
+  messageDate = formattedDate;
 
-  let text = ''
   if (message.from !== me.id) {
-    text = `<span style="color:red">${sender.user.name} <span style="color:red;">(${formattedDate}): <b>${message.body.text}</b></span>`
+    text = `<span style="color:red">${sender.user.name} (${formattedDate}): <b>${message.body.text}</b></span>`;
   } else {
-    text = `me (${formattedDate}): <b>${message.body.text}</b>`
+    text = `me (${formattedDate}): <b>${message.body.text}</b>`;
   }
 
-  messageFeed.innerHTML = messageFeed.innerHTML + text + '<br />';
+  return text + '<br />';
+
 }
